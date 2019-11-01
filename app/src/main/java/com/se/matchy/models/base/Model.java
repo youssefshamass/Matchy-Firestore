96ad55a8dfb76995baa392cc31f6968432592808
Youssef Shamass <my.shamass@gmail.com>
2019-11-01 14:26:13 +0200

    Initial commit

---
+++ app/src/main/java/com/se/matchy/models/base/Model.java
@@ -0,0 +1,20 @@
+package com.se.matchy.models.base;
+
+
+import java.io.Serializable;
+
+/**
+ * Useless model class
+ */
+public class Model implements Serializable {
+
+
+    /**
+     * Useless method
+     * @param takes what it takes
+     * @return return the save variable received.
+     */
+    public int getStatus(int takes) {
+     return takes;
+    }
+}
